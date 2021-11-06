42467c370ca7a4044cbc3bc676b68a54b4a29fde
Ryan Kotsovolos <rkotsovolos@gmail.com>
2021-11-06 16:25:08 -0700

    commit tree 31cd6a0bc01c6de2f47e65dfc01672775da4ef77

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import { useState } from 'react';
 
 function Item({ name, category }) {
+
+  const [added, setAdded] = useState(true)
+
+  function addToCartClick() {
+    setAdded((added) => !added )
+  }
+
+  const cartValue = added ? "Add to Cart" : "Remove From Cart";
+  const classValue = added ? "" : "in-cart";
+  const buttonClassName = added ? "add" : "remove";
+
   return (
-    <li className="">
+    <li className={classValue}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClassName} onClick={addToCartClick}>{cartValue}</button>
     </li>
   );
 }
